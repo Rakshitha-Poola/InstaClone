ba04f4e03a43ecae8240b574f39d4438cb2fd865
Rakshitha-Poola <poolarakshitha@gmail.com>
2025-07-24 09:47:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  server: {
-    proxy: {
-      '/apis': {
-        target: 'https://apis.ccbp.in',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/apis/, ''),
-      },
-    },
-  },
+
 })
